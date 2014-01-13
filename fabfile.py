739a22fab8b7f069f8dc15249b6c9bcb1162bc23
Dedi Purwanto <kecebongsoft@gmail.com>
2014-01-14 07:38:06 +0800

    New build

--- fabfile.py
+++ fabfile.py
@@ -1,4 +1,6 @@
 from fabric.api import local, lcd
+from bs4 import BeautifulSoup
+from slugify import slugify
 
 def build():
     local("pelican . -o build/ -s pelican.conf.py")
@@ -16,3 +18,19 @@ def deploy():
 
     local("git add build")
     local("git commit --all --message 'New build'")
+
+def import_tumblr():
+    doc = BeautifulSoup(open('tumblr_kecebongsoft.xml', 'r'))
+
+    for item in doc.findAll('item'):
+        date = item.find("wp:post_date").text
+        title = item.find("title").text.encode('ascii', 'ignore')
+        content = item.find("content:encoded").text.encode('ascii', 'ignore')
+
+        if title is None or len(title) == 0:
+            title = date
+
+        f = open('tumblr/%s.md' % slugify(title), 'w')
+
+        f.write("title:%s\ndate:%s\nstatus:draft\n\n%s" % (title.encode('ascii', 'ignore'), date, content.encode('ascii', 'ignore')))
+
