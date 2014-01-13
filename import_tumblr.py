5ac9295a02381797b417255b13a1c86418004bc0
Dedi Purwanto <kecebongsoft@gmail.com>
2014-01-13 17:25:03 +0800

    Fix encode errors

--- import_tumblr.py
+++ import_tumblr.py
@@ -3,29 +3,14 @@ from slugify import slugify
 
 doc = BeautifulSoup(open('tumblr_kecebongsoft.xml', 'r'))
 
-ENCODINGS = [ 'utf8', 'gb18030', 'ascii' 'latin1', ]
-def unicode(s):
-    for encoding in ENCODINGS:
-        try:
-            s = s.encode(encoding)
-            return s
-        except:
-            pass
-    return u"CANt encode"
-
 for item in doc.findAll('item'):
     date = item.find("wp:post_date").text
-    title = item.find("title").text
-    content = item.find("content:encoded").text
+    title = item.find("title").text.encode('ascii', 'ignore')
+    content = item.find("content:encoded").text.encode('ascii', 'ignore')
 
     if title is None or len(title) == 0:
         title = date
 
     f = open('tumblr/%s.md' % slugify(title), 'w')
 
-    title = unicode(title)
-    content = unicode(content)
-
-    f.write("title:%s\ndate:%s\nstatus:draft\n\n%s" % (unicode(title), date, unicode(content)))
-
-
+    f.write("title:%s\ndate:%s\nstatus:draft\n\n%s" % (title.encode('ascii', 'ignore'), date, content.encode('ascii', 'ignore')))
--- tumblr/flashdevelop-color-scheme.md
+++ tumblr/flashdevelop-color-scheme.md
@@ -2,4 +2,5 @@ title:FlashDevelop color scheme
 date:2010-11-28 21:49:00
 status:draft
 
-CANt encode
+<p><a href="http://kecebongsoft.files.wordpress.com/2010/11/fd.jpg"><img src="http://media.tumblr.com/tumblr_lcmthodV2p1qajazg.jpg" /></a></p>&#13;
+<p>I've got a nice visual studio color scheme from somewhere, and I applied it (manually) to flashdevelop. It somehow looked like borland syntax coloring, anyway, it has dark background with shiny colored font. Download my FD color scheme from here:<a href="http://www.4shared.com/file/M577IxmY/FD_Borland_Color_Scheme.html">http://www.4shared.com/file/M577IxmY/FD_Borland_Color_Scheme.html</a></p> 
